c0a9a869485112d6524124962a28bdb9035d25dc
Yonguk Kim <yonguk99@naver.com>
2022-07-26 10:52:16 +0900

    4 commit

---
+++ 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    if (s[0] != ' ' && (s[0] >= 'a' && s[0] <= 'z')) s[0] -= 32;
+    if (s.size() == 1) return s;
+    for (int i = 1; i < s.size(); i++) {
+        if (s[i] == ' ' && i + 1 <= s.size()) {
+            i++;
+            if (s[i] >= 'a' && s[i] <= 'z') s[i] -= 32;
+            else if (s[i] == ' ') {
+                i--;
+                continue;
+                //연습 테스트입니다111.
+            }
+        }
+        else if (s[i] >= 'A' && s[i] <= 'Z') s[i] += 32;
+    }
+    return s;
+}
+int main() {
+    solution("for    the last week");
+}
